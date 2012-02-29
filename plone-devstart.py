3081a1df6fd39897d8a7e9bf08c0f96497d463f5
Martin Aspeli <optilude@gmail.com>
2012-02-29 22:14:25 +0000

    More work in progress

--- plone-devstart.py
+++ plone-devstart.py
@@ -20,12 +20,19 @@ You will be asked several questions along the way
 import os
 import os.path
 import sys
+import httplib
+import urlparse
+import urllib2
+import subprocess
+
+# Version of this script
+devstart_version = "0.1"
 
 # Base Python version and skeleton location for each base Plone version (to minor version)
 plone_versions = {
     '4.1' : {
-        'python': '2.6',
-        'skeleton': 'https://raw.github.com/optilude/plone-devstart/closet/plone-4.1.zip',
+        'python_version': '2.6',
+        'skeleton_url': 'https://raw.github.com/optilude/plone-devstart/closet/plone-4.1.zip',
     },
 }
 default_version = '4.1'
@@ -38,32 +45,75 @@ config = {
 
 # Utilities
 
-def download(url, directory):
-    """Download the given file into the directory
+def download(url, directory, filename):
+    """Download the given file into the given (or current) directory
     """
 
-def run(command):
+    # Thanks to http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+    u = urllib2.urlopen(url)
+    f = open(os.path.join(directory, filename), 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "> Downloading: %s (%d bytes)" % (filename, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+    print
+    print "Done"
+    f.close()
+
+def run(command, *args):
     """Run the given command
     """
+    print "> Running:", command, ' '.join(args)
+    return subprocess.call([command] + list(args)) == 0
 
-def ask(prompt, default):
+def ask(prompt, default=None):
     """Ask a question and return the response entered by the user
     """
+    if default:
+        prompt = prompt + " [%s] " % default
+    answer = raw_input(prompt)
+    if not answer and default:
+        answer = default
+    return answer
 
 def check_url(url):
     """Check to see if the given URL exists
     """
 
+    split = urlparse.urlsplit(url)
+    connection = httplib.HTTPConnection(split.netloc, port=split.port)
+    connection.request('HEAD', split.path)
+    response = connection.getresponse()
+    return response.status == 200
+
 def get_base_version(version):
     """Turn a specific Plone or Python version into a base version
     """
+    return '.'.join(version.split('.')[:2])
 
 # Verification
 
-def check_python_version(plone_version):
+def check_python_version(version_config):
     """Given an intended Plone version, determine if the current Python version
     is acceptable
     """
+    python_version = version_config['python_version'].split('.')
+    for i, e in enumerate(python_version):
+        if i >= len(sys.version_info) or str(sys.version_info[i]) != e:
+            return False
+    return True
 
 # Execution
 
@@ -73,9 +123,10 @@ def main():
     directory =  os.getcwd()
     if len(args) > 1:
         directory = args[1]
+    directory = os.path.abspath(directory)
 
     print
-    print "Welcome to plone-devstart."
+    print "Welcome to plone-devstart (version %s)" % devstart_version
     print "Press Ctrl+C any time to abort"
     print
 
@@ -89,52 +140,110 @@ def main():
     print "You can use a more specific revision of any of these, e.g. 4.1.2"
     print
 
-    version = raw_input("Enter a Plone version number [%s] " % default_version)
+    version = ask("Enter a Plone version number", default_version)
     base_version = get_base_version(version)
 
     while base_version not in plone_versions:
+        print
         print "plone-devstart does not know what to do with this version."
-        print "Known versions start with one of: ", ", ".join(sorted(plone_versions.keys()))
+        print "Known versions start with one of:", ", ".join(sorted(plone_versions.keys()))
         print "Please try again or press Ctrl+C to abort."
         print
 
-        version = raw_input("Enter a Plone version number [%s] " % default_version)
+        version = ask("Enter a Plone version number", default_version)
         base_version = get_base_version(version)
 
-    kgs_url = config['plone_kgs_url'] % version
+    version_config = plone_versions[base_version]
+
+    kgs_url = config['plone_kgs_url'] % {'plone_version': version}
+    print
+    print "Checking for known good versions set at", kgs_url, "..."
     if not check_url(kgs_url):
-        print "Warning: No known good set found at", kgs_url
+        print
+        print "** Warning: No known good set found at"
+        print kgs_url
         print "Plone build will likely fail."
         print
-        input("Press Enter to continue, or Ctrl+C to abort")
+        ask("Press Enter to continue, or Ctrl+C to abort")
+    else:
+        print "Done"
 
-    if not check_python_version(version):
-        print "Warning: The current Python version is not known to work with Plone ", version
-
-        python_version = plone_versions.get(base_version, {}).get('python_version', None)
-        if python_version is not None:
-            print "The expected Python version is ", python_version        
-
+    if not check_python_version(version_config):
+        print
+        print "** Warning: The current Python version is not known to work with Plone", version
+        print "The expected Python version is", version_config['python_version'], "but this script is being run with Python"
+        print "version", '.'.join([str(s) for s in sys.version_info])
         print "Plone build may fail."
         print
-        input("Press Enter to continue, or Ctrl+C to abort")
+        ask("Press Enter to continue, or Ctrl+C to abort")
 
+    print
+    print "Creating build in directory", directory
+    print
+    create_directory(directory)
+
+    print
+    print "Creating virtual Python environment"
+    print
+    create_virtualenv(directory)
+
+    print
+    print "Obtaining skeleton buildout"
+    print
+    create_buildout(directory, version, version_config)
+
+    print
+    print "Bootstrapping buildout"
+    print
+    bootstrap(directory)
 
 def create_directory(directory):
-    """Ensure the given directory exists
+    """Create the build directory if necessary
     """
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
 def create_virtualenv(directory):
     """Create a virtualenv in the given directory
     """
+    cwd = os.getcwd()
 
-def create_buildout(directory, data):
+    download(config['virtualenv_url'], directory, 'virtualenv.py')
+
+    os.chdir(directory)
+    run(sys.executable, 'virtualenv.py', '--no-site-packages', directory)
+    os.chdir(cwd)
+
+def create_buildout(directory, plone_version, version_config):
     """Create a new buildout in the given directory
     """
+
+    cwd = os.getcwd()
+
+    if os.path.exists(os.path.join(directory, 'buildout.cfg')):
+        print
+        print "** Warning: It looks like there is already a buildout.cfg file here."
+        print "plone-devstart will not override it or recreate any other files"
+        return
+
+    # Download
+    download(version_config['skeleton_url'], directory, 'buildout-skeleton.zip')
+
+    # Unzip and update file contents
+    
+
+    os.chdir(directory)
+    os.chdir(cwd)
 
 def bootstrap(directory):
     """Bootstrap the buildout in the given directory
     """
+    cwd = os.getcwd()
+    download(config['bootstrap_url'], directory, 'bootstrap.py')
+
+    os.chdir(directory)
+    run(os.path.join(directory, 'bin', 'python'), 'bootstrap.py')
+    os.chdir(cwd)
 
 if __name__ == '__main__':
     main()
